8f762a68377204127ea9490d0e0858e45a561cd8
tehreemt <61510365+tehreemt@users.noreply.github.com>
2020-05-05 12:12:34 -0400

    Update mongoCollections.js

--- config/mongoCollections.js
+++ config/mongoCollections.js
@@ -12,32 +12,12 @@ const getCollectionFn = collection => {
     }
     return _col;
   };
-};const dbConnection = require("./mongoConnection");
-
-/* This will allow you to have one reference to each collection per app */
-/* Feel free to copy and paste this this */
-const getCollectionFn = collection => {
-  let _col = undefined;
-
-  return async () => {
-    if (!_col) {
-      const db = await dbConnection();
-      _col = await db.collection(collection);
-    }
-    return _col;
-  };
 };
 
 /* Now, you can list your collections here: */
 module.exports = {
   games: getCollectionFn("games"),
  users:getCollectionFn("users"),
- reviews:getCollectionFn("reviews")
-};
-
-
-/* Now, you can list your collections here: */
-module.exports = {
-  games: getCollectionFn("games"),
- 
+ reviews:getCollectionFn("reviews"),
+ comments:getCollectionFn("comments")
 };
