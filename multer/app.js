4c8b8ee29538618fc785413f16071e397e0445d7
tehreemt <61510365+tehreemt@users.noreply.github.com>
2020-05-11 12:04:26 -0400

    Add files via upload

---
+++ multer/app.js
@@ -0,0 +1,59 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+const express = require("express");
+
+const app = express();
+const httpServer = http.createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+httpServer.listen(3000, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+// put the HTML file containing your form in a directory named "public" (relative to where this script is located)
+app.get("/", express.static(path.join(__dirname, "./public")));
+
+const multer = require("multer");
+
+const handleError = (err, res) => {
+  res
+    .status(500)
+    .contentType("text/plain")
+    .end("Oops! Something went wrong!");
+};
+
+const upload = multer({
+  dest: "/path/to/temporary/directory/to/store/uploaded/files"
+});
+
+
+app.post(
+  "/upload",
+  upload.single("file" /* name attribute of <file> element in your form */),
+  (req, res) => {
+    const tempPath = req.file.path;
+  const targetPath = path.join(__dirname, "./uploads/name.png");
+    if (path.extname(req.file.originalname).toLowerCase() === ".png"||".jpg") {
+      fs.rename(tempPath, targetPath, err => {
+        if (err) return handleError(err, res);
+
+        res.status(200)
+        .contentType("text/plain")
+        .end("File uploaded!");
+    });
+  } else {
+    fs.unlink(tempPath, err => {
+      if (err) return handleError(err, res);
+
+      res
+        .status(403)
+        .contentType("text/plain")
+        .end("Only .png & .jpg files are allowed!");
+    });
+  }
+}
+);
+
